7d1cd4a3640315173a939016746d613d2f03548d
Victor Cruz <victorgtdacruz@outlook.com>
2025-04-30 11:28:27 -0300

    Add auth

--- app/src/main/java/br/com/wave/pokedex_challenge_android/PokeApiService.kt
+++ app/src/main/java/br/com/wave/pokedex_challenge_android/PokeApiService.kt
@@ -3,14 +3,20 @@ package br.com.wave.pokedex_challenge_android
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 
 interface PokeApiService {
     @GET("pokemon?limit=151")
-    suspend fun getPokemonList(): PokemonListResponse
+    suspend fun getPokemonList(
+        @Header("X-Authorization") token: String = "k45xAxvhWH9xyRGLnhDWvHCSsMEadDX4OiRko03wOw588"
+    ): PokemonListResponse
 
     @GET("pokemon/{name}")
-    suspend fun getPokemonDetails(@Path("name") name: String): PokemonDetails
+    suspend fun getPokemonDetails(
+        @Path("name") name: String,
+        @Header("X-Authorization") token: String = "k45xAxvhWH9xyRGLnhDWvHCSsMEadDX4OiRko03wOw588"
+    ): PokemonDetails
 }
 
 data class PokemonListResponse(
